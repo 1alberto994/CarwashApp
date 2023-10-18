e9641d99ecfc6e43796be6de1203bcb9a26b5800
Giardina Alberto <alby94ct@gmail.com>
2023-10-18 13:15:24 +0200

    correzioni punto uno

--- CarWashLibrary/Company.cs
+++ CarWashLibrary/Company.cs
@@ -2,40 +2,22 @@ using System.Text;
 public class Company
 {
     private Dictionary<string,Implant> _implants;
-    private Dictionary<string,int> _counters;
+  
     public string ViewImplant()
     {
-          StringBuilder result= new StringBuilder();
-        result.AppendLine("SelfImplant:");
+        string autoImplant = "";
+        string selfImplant="";
         foreach ( var implants in _implants)
         {
             if (implants.Value is SelfImplant)
             {
-                string code=implants.Key;
-                string CurrentState=implants.Value.CurrentState;//tostring?
-                int counter= 0;
-                if (_counters.ContainsKey(code))
-                {
-                    counter = _counters[code];
-                }
-                result.AppendLine($"Code:{code},Type:Self,State:{CurrentState},Counter:{counter}");
+                selfImplant += implants.Value.ToString();
             }
-        }
-
-        foreach ( var implants in _implants)
-        {
-            if (implants.Value is AutoImplant)
+            else
             {
-                string code=implants.Key;
-                string CurrentState=implants.Value.CurrentState;//tostring?
-                int counter= 0;
-                if (_counters.ContainsKey(code))
-                {
-                    counter = _counters[code];
-                }
-                result.AppendLine($"Code:{code},Type:Auto,State:{CurrentState},Counter:{counter}");
+                autoImplant +=implants.Value.ToString();
             }
         }
-        return result.ToString();
+        return selfImplant + autoImplant;
     }
 }
