3bc87d44c0f807aba178fcbf0869f6e4f108e585
Giardina Alberto <alby94ct@gmail.com>
2023-10-18 16:33:10 +0200

    Punto 3 complete

--- CarWashLibrary/Company.cs
+++ CarWashLibrary/Company.cs
@@ -2,13 +2,13 @@ using System.Xml.XPath;
 
 public class Company
 {
-    private Dictionary<string,Implant> _implants;
-  
+    private Dictionary<string, Implant> _implants;
+
     public string ViewImplant()
     {
         string autoImplant = "";
-        string selfImplant="";
-        foreach ( var implants in _implants)
+        string selfImplant = "";
+        foreach (var implants in _implants)
         {
             if (implants.Value is SelfImplant)
             {
@@ -16,30 +16,43 @@ public class Company
             }
             else
             {
-                autoImplant +=implants.Value.ToString();
+                autoImplant += implants.Value.ToString();
             }
         }
         return selfImplant + autoImplant;
     }
     public string ViewStautusCounter(string ID)
     {
-            Implant implant=_implants[ID];
-            string result="ID"+implant.ID;
-            result+="Current state:"+ implant.CurrentState;
-        
-            
-            if (implant is AutoImplant)
-            {
-                AutoImplant autoImplant=(AutoImplant)implant;
-                Console.WriteLine("Counter wash:" + autoImplant.CountWash);
-            }
-            else
-            {
-                SelfImplant selfImplant=(SelfImplant)implant;
-                Console.WriteLine("Compressor counter:"+ selfImplant.CompressorCounter + "\n" + "Washing lance counter:"+ selfImplant.WashingCounter + "\n"+ "Brush waxing counter:"+ selfImplant.BrushWaxingCounter);
-            }
+        Implant implant = _implants[ID];
+        string result = "ID" + implant.ID;
+        result += "Current state:" + implant.CurrentState;
+
+
+        if (implant is AutoImplant)
+        {
+            AutoImplant autoImplant = (AutoImplant)implant;
+            Console.WriteLine("Counter wash:" + autoImplant.CountWash);
+        }
+        else
+        {
+            SelfImplant selfImplant = (SelfImplant)implant;
+            Console.WriteLine("Compressor counter:" + selfImplant.CompressorCounter + "\n" + "Washing lance counter:" + selfImplant.WashingCounter + "\n" + "Brush waxing counter:" + selfImplant.BrushWaxingCounter);
+        }
         return result;
-        
+
     }
-    
+    public List<Implant> SearchSatusMaintenance()
+    {
+        List<Implant> maintenance = new();
+        foreach (var implant in _implants.Values)
+        {
+            if (implant.CurrentState == Implant.States.M) { maintenance.Add(implant); }
+        }
+
+            return maintenance;
+
+
+
+    }
+
 }
--- CarWashLibrary/Implant.cs
+++ CarWashLibrary/Implant.cs
@@ -1,30 +1,14 @@
 public class Implant
 {
-    private enum States { O,M,B};
+    public enum States { O, M, B };
     private States _currentState;
     private double _costSinglewash;
-    private List<Tuple<DateOnly,States>>_logPrevStates;
+    private List<Tuple<DateOnly, States>> _logPrevStates;
     private string _id;
 
-    public string ID {get=>_id;}
-     public string CurrentState
-    {
-        get
-        {
-            switch (_currentState)
-            {
-                case States.O:
-                    return "Operative";
-                case States.M:
-                    return "In Maintenance";
-                case States.B:
-                    return "Broken Down";
-                default:
-                    throw new NotImplementedException("Invalid states");
-            }
-        }
-    }
-    public double CostSingleWash{get=>_costSinglewash;}
-    
-    
+    public string ID { get => _id; }
+    public States CurrentState { get => _currentState; }
+    public double CostSingleWash { get => _costSinglewash; }
+
+
 }
