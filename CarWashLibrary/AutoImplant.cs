993800806014823b161bed76485154bca58ae39a
Giardina Alberto <alby94ct@gmail.com>
2023-10-18 10:18:00 +0200

    add self implant & auto implant

--- CarWashLibrary/AutoImplant.cs
+++ CarWashLibrary/AutoImplant.cs
@@ -1,10 +1,5 @@
 public class AutoImplant : Implant
 {
-
-    private uint _id;
-    // status[] ?? come implementarlo? => con l'enum alla chiamata del metodo "changeState" non avrei dove prenderlo (se definito dentro la classe)
-    // private ?? [] oldState; 
-    private double cost;
-
-    // public int howManyTimeBroken() { /* Not implemented */ }
+    private int _countWash;
+    public int CountWash{get=>_countWash;}
 }
--- CarWashLibrary/Implant.cs
+++ CarWashLibrary/Implant.cs
@@ -1,21 +1,30 @@
-
-
-
 public class Implant
 {
     private enum States { O,M,B};
     private States _currentState;
     private double _costSinglewash;
-    private List<Tuple<DateTime,States>>_Logprevstates;
+    private List<Tuple<DateOnly,States>>_logPrevStates;
     private string _id;
 
     public string ID {get=>_id;}
-    public States CurrentState
+     public string CurrentState
     {
         get
         {
-            
+            switch (_currentState)
+            {
+                case States.O:
+                    return "Operative";
+                case States.M:
+                    return "In Maintenance";
+                case States.B:
+                    return "Broken Down";
+                default:
+                    throw new NotImplementedException("Invalid states");
+            }
         }
     }
+    public double CostSingleWash{get=>_costSinglewash;}
+    
     
 }
